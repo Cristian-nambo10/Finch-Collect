737fcb99019cb41495cf841f9b5b0ced19a86c1d
Cristian Nambo <nambocristian10@gmail.com>
2022-07-06 23:24:22 -0500

    wip: admin added and data from psql

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
+import imp
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Finch(models.Model):
+    name = models.CharField(max_length=80)
+    family = models.CharField(max_length=100)
+    description = models.TextField(max_length=300)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from main_app.models import Finch
 
 # Create your views here.
 def home(request):
@@ -9,19 +10,23 @@ def about(request):
     return render(request, 'about.html')
 
 
-class Finch:
-    def __init__(self, name, family, age, description):
-        self.name = name
-        self.family = family
-        self.order = age
-        self.description = description
+# class Finch:
+#     def __init__(self, name, family, age, description):
+#         self.name = name
+#         self.family = family
+#         self.age = age
+#         self.description = description
 
-finch = [
-    Finch('Test', 'hi', 'tall', 'Small and cute'),
-    Finch('test2', 'no', 'short', "Small but fierce"),
-    Finch('test3', 'lin', 'average', 'Big and scared'),
-]
+# finch = [
+#     Finch('Test', 'hi', 7, 'Small and cute'),
+#     Finch('test2', 'no', 5, "Small but fierce"),
+#     Finch('test3', 'lin', 3, 'Big and scared'),
+# ]
 
 def finch_index(request):
-    # finches = Finch.objects.all()
+    finch = Finch.objects.all()
     return render(request, 'finches/index.html', {'finch': finch})
+
+def finch_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', { 'finch': finch })
