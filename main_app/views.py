e21099b3a05c778c11229317ea057a60f85f0a17
Cristian Nambo <nambocristian10@gmail.com>
2022-07-06 22:28:41 -0500

    wip: posted temp data

--- main_app/views.py
+++ main_app/views.py
@@ -7,3 +7,21 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+
+class Finch:
+    def __init__(self, name, family, age, description):
+        self.name = name
+        self.family = family
+        self.order = age
+        self.description = description
+
+finch = [
+    Finch('Test', 'hi', 'tall', 'Small and cute'),
+    Finch('test2', 'no', 'short', "Small but fierce"),
+    Finch('test3', 'lin', 'average', 'Big and scared'),
+]
+
+def finch_index(request):
+    # finches = Finch.objects.all()
+    return render(request, 'finches/index.html', {'finch': finch})
